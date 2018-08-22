7e36c185eb77003c67dca6569149a64d8d9ad175
sugeethkumar <sugeethpro@hotmail.com>
2018-08-22 20:59:19 +0200

    Added @IBoutlet and @IBaction, func interve stack added.

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -10,16 +10,51 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var square1: UIButton!
+    
+    @IBOutlet weak var square2: UIButton!
+    
+    @IBOutlet weak var rectangle: UIButton!
+    
+    @IBOutlet weak var twoSquare: UIStackView!
+    
+    @IBOutlet weak var rectangleStack: UIStackView!
+    
+    @IBOutlet weak var rectangleAndSquare: UIStackView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       interveStack()
     }
+    
+    
+    @IBAction func layout1() {
+      
+    }
+    
+    @IBAction func layout2() {
+    
+    }
+    
+    @IBAction func layout3() {
+    }
+    
+    @IBAction func squareLeft() {
+    }
+    
+    @IBAction func squareRight() {
+    }
+    
+    @IBAction func rectangle1() {
+    }
+    
+    func interveStack(){
+         rectangleAndSquare.addArrangedSubview(self.rectangleAndSquare.subviews[0])
+    }
+    
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+   
 
 
 }
