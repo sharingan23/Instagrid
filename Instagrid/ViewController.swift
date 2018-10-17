47cb86209eb0667d7c9505c4d2fffc90206f8d7a
sugeethkumar <sugeethpro@hotmail.com>
2018-10-18 01:06:27 +0200

    Action of layout, is changing layouts, button are working fine.

--- Instagrid/ViewController.swift
+++ Instagrid/ViewController.swift
@@ -33,12 +33,14 @@ class ViewController: UIViewController {
         let image = UIImage(named: "Combined Shape")
         button.setImage(image , for: .normal)
         //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
         //Create Button 2
         let button2 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
         button2.backgroundColor = UIColor.white
         let image2 = UIImage(named: "Combined Shape")
         button2.setImage(image2 , for: .normal)
         //button2.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
         //Create Button 3
         let button3 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
         let image3 = UIImage(named: "Combined Shape")
@@ -46,59 +48,79 @@ class ViewController: UIViewController {
         button3.setImage(image3 , for: .normal)
         //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
         
-       
-
+        //Remove all subview and add buttons
+        resetViews()
+        topview.addArrangedSubview(button)
+        bottomView.addArrangedSubview(button2)
+        bottomView.addArrangedSubview(button3)
+    }
+    
+    @IBAction func layout2() {
+        //Create Button 1
+        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        button.backgroundColor = UIColor.white
+        let image = UIImage(named: "Combined Shape")
+        button.setImage(image , for: .normal)
+        //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
         
+        //Create Button 2
+        let button2 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        button2.backgroundColor = UIColor.white
+        let image2 = UIImage(named: "Combined Shape")
+        button2.setImage(image2 , for: .normal)
+        //button2.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
         
+        //Create Button 3
+        let button3 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        let image3 = UIImage(named: "Combined Shape")
+        button3.backgroundColor = UIColor.white
+        button3.setImage(image3 , for: .normal)
+        //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
         
+        //Remove all subview and add buttons
         resetViews()
         topview.addArrangedSubview(button)
         topview.addArrangedSubview(button2)
         bottomView.addArrangedSubview(button3)
-        topview.translatesAutoresizingMaskIntoConstraints = false
-        bottomView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let widtConstraint = NSLayoutConstraint(
-            item: button,
-            attribute: NSLayoutAttribute.width,
-            relatedBy: NSLayoutRelation.equal,
-            toItem: button2,
-            attribute: NSLayoutAttribute.width,
-            multiplier: 1.0,
-            constant: 0
-        )
-        topview.addConstraint(widtConstraint)
-        
     }
-    
-    @IBAction func layout2() {
-        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
-        button.titleLabel?.text = "Genious Team"
-        button.backgroundColor = UIColor.blue
-        let button2 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
-        button2.titleLabel?.text = "Genious Team"
-        button2.backgroundColor = UIColor.red
-        let button3 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
-        button3.titleLabel?.text = "Genious Team"
-        button3.backgroundColor = UIColor.yellow
-
-        resetViews()
-        /*topview.addArrangedSubview(button)
-        bottomView.addArrangedSubview(button2)
-        bottomView.addArrangedSubview(button3)
- */
-       
-       
-    }
-   /*
+   
     @IBAction func layout3() {
+        //Create Button 1
+        let button = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        button.backgroundColor = UIColor.white
+        let image = UIImage(named: "Combined Shape")
+        button.setImage(image , for: .normal)
+        //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
+        //Create Button 2
+        let button2 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        button2.backgroundColor = UIColor.white
+        let image2 = UIImage(named: "Combined Shape")
+        button2.setImage(image2 , for: .normal)
+        //button2.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
+        //Create Button 3
+        let button3 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        let image3 = UIImage(named: "Combined Shape")
+        button3.backgroundColor = UIColor.white
+        button3.setImage(image3 , for: .normal)
+        //button.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
+        //Create Button 4
+        let button4 = UIButton(frame: CGRect(x: 10, y: 10, width: 50, height: 50))
+        button4.backgroundColor = UIColor.white
+        let image4 = UIImage(named: "Combined Shape")
+        button4.setImage(image4 , for: .normal)
+        //button4.addTarget(self, action: "<#T##Selector#>", for: UIControlEvents.touchUpInside)
+        
+        //Remove all subview and add buttons
         resetViews()
-        topview.addArrangedSubview(myButton)
-        topview.addArrangedSubview(myButton)
-        bottomView.addArrangedSubview(myButton)
-        bottomView.addArrangedSubview(myButton)
+        topview.addArrangedSubview(button)
+        topview.addArrangedSubview(button2)
+        bottomView.addArrangedSubview(button3)
+        bottomView.addArrangedSubview(button4)
     }
- */
+ 
     func resetViews () {
         for view in topview.arrangedSubviews {
             view.removeFromSuperview()
